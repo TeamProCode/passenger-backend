0143f3a5c08426cbf7641268371694f6ce8ecf8a
EWinkeljohn <emcwink@gmail.com>
2024-01-08 13:38:08 -0800

    user validations complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,14 @@ RSpec.describe User, type: :model do
         expect(user.errors[:password]).to_not be_empty
     end
 
+    it "should not create a user without a password confirmation" do
+        user = User.create(
+            email: 'test@example.com',
+            password: 'password',
+        ) 
+        expect(user.errors[:password_confirmation]).to_not be_empty
+    end
+
     it "should not allow users with duplicate emails" do
         User.create(
             email: 'test@example.com',
@@ -38,5 +46,14 @@ RSpec.describe User, type: :model do
             password_confirmation: 'password'
         )
         expect(user.errors[:email]).to_not be_empty
+    end
+
+    it "should not allow password and password_confirmation to be different" do
+        user = User.create(
+            email: 'test@example.com',
+            password: 'password',
+            password_confirmation: 'passw0rd'
+        )
+        expect(user.errors[:password_confirmation]).to_not be_empty
     end
 end
