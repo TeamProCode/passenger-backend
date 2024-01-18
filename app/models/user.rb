65b3d250d3b8f773d726cc32e7eaca872fa6c8a9
EWinkeljohn <emcwink@gmail.com>
2024-01-18 09:23:37 -0800

    backend good

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist 
   has_many :destinations
   has_many :photos, through: :destinations
-  validates :email, presence: true
-  validates :email, uniqueness: true
-  # validates the presence of password when id is nil aka the account is new and does not have an id yet
-  # -> lamdba syntax: creates an anonymous function that checks for existence of id; if true, password validation applied
-  validates :password, presence: true
-  validates :password, length: { minimum: 8 }
+  # validates :email, presence: true
+  # validates :email, uniqueness: true
+  # # validates the presence of password when id is nil aka the account is new and does not have an id yet
+  # # -> lamdba syntax: creates an anonymous function that checks for existence of id; if true, password validation applied
+  # validates :password, presence: true
+  # validates :password, length: { minimum: 8 }
   # validates :password, confirmation: true
 end
