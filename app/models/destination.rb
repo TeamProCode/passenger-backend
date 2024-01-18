3e553d2261ba50c11474c8db52660b32633207bd
EWinkeljohn <emcwink@gmail.com>
2024-01-17 20:08:06 -0800

    trying to fix fetch calls

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,5 +1,5 @@
 class Destination < ApplicationRecord
     belongs_to :user
     has_many :photos
-    validates :location, :climate, :local_language, :image, :description, :user_id, presence: true
+    validates :location, :climate, :local_language, :image, :description, presence: true
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://passenger-front.onrender.com'
+      # origins 'https://passenger-front.onrender.com'
+      origins 'http://localhost:3001'
       resource '*',
       headers: ["Authorization"],
       expose: ["Authorization"],
